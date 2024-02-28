8120a5dfc485c7d55b3005418bc2632cc75d8c6c
AnnaKupriienko <kupriienko16@gmail.com>
2024-02-28 14:02:52 +0000

    save

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,8 +1,6 @@
 const form = document.querySelector(".feedback-form")
 const localStorageKey = "feedback-form-state"
-
 form.addEventListener('input', (event) => {
-<<<<<<< Updated upstream
   localStorage.setItem('localStorageKey', JSON.stringify({
   email: form.elements.email.value.trim(),
   message: form.elements.message.value.trim()
@@ -18,22 +16,6 @@ form.addEventListener('input', (event) => {
       console.log(formData)
     }
     localStorage.removeItem(localStorageKey)
-=======
-        localStorage.setItem(localStorageKey, JSON.stringify({
-            email: form.elements.email.value.trim(),
-            message: form.elements.message.value.trim()
-        }));
-    });
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = {
-      email: form.elements.email.value.trim(),
-      message: form.elements.message.value.trim()
-    };
-    if (formData) {
-      console.log(formData);
-    }
->>>>>>> Stashed changes
       form.reset()
   });
+
