13c0db547a77bd6dc71fc2170f928db982468c85
AnnaKupriienko <kupriienko16@gmail.com>
2024-02-28 14:01:31 +0000

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,21 +1,21 @@
 const form = document.querySelector(".feedback-form")
 const localStorageKey = "feedback-form-state"
 form.addEventListener('input', (event) => {
-    if (event.target.tagName.toLowerCase() === 'input') {
-        localStorage.setItem('feedback-form', JSON.stringify({
-            email: document.querySelector('input[name=email]').value,
-            message: document.querySelector('textarea[name=message]').value
+  localStorage.setItem('localStorageKey', JSON.stringify({
+  email: form.elements.email.value.trim(),
+  message: form.elements.message.value.trim()
         }));
-    }
-});
-
+    });
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-      localStorage.clear()
     const formData = {
-     email: document.querySelector('input[name=email]').value,
-     message: document.querySelector('textarea[name=message]').value
+  email: form.elements.email.value.trim(),
+  message: form.elements.message.value.trim()
     };
+    if (formData) {
+      console.log(formData)
+    }
+    localStorage.removeItem(localStorageKey)
       form.reset()
   });
 
